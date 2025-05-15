4eaef67ba3e4bdfaefad1e37f63dd95e58b5b8df
venkatesh <veerojuvenkatesh5@gmail.com>
2025-05-15 15:19:03 +0200

    OAuth2 login using GitHub
    - OAuth2 login using GitHub

---
+++ src/main/java/dev/code/social_login/Homecontroller.java
@@ -0,0 +1,16 @@
+package dev.code.social_login;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Homecontroller {
+    @GetMapping("/")
+    public String home(){
+        return "Hello, Home";
+    }
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello, Secured";
+    }
+}
